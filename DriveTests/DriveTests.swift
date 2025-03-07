312ffc77c45aedbf52b2e03ebeaff4af0361b348
seanyse <seanyan0915@gmail.com>
2025-03-07 15:29:28 -0500

    Initial Commit

---
+++ DriveTests/DriveTests.swift
@@ -0,0 +1,17 @@
+//
+//  DriveTests.swift
+//  DriveTests
+//
+//  Created by Sean Yan on 3/7/25.
+//
+
+import Testing
+@testable import Drive
+
+struct DriveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ DriveUITests/DriveUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  DriveUITestsLaunchTests.swift
+//  DriveUITests
+//
+//  Created by Sean Yan on 3/7/25.
+//
+
+import XCTest
+
+final class DriveUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
